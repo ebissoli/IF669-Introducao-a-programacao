d6fb3df767a640167ac0a43695f449f35e7f8693
Enzo Bissoli <enzo.gurgelbissoli@ufpe.br>
2021-06-14 14:27:33 -0300

    Foi meu bro :)

--- Semana3/Lista2.1/diasFaltam.c
+++ Semana3/Lista2.1/diasFaltam.c
@@ -4,29 +4,36 @@ int main(){
     int mes1Temp = 1;int flag;
     scanf("%d%*c%d%*c%d\n%d%*c%d%*c%d", &dia1, &mes1, &ano1, &dia2, &mes2, &ano2);
     diasFaltam = 0;
-    flag = 1;
-    while(ano1<ano2) {
-        bissexto = ((ano1%4==0 && ano1%100==0) || ano1%400==0)?1:0;
-        diasFaltam+=365+bissexto;
-        ano1++;
-    }
-    bissexto = ((ano1%4==0 && ano1%100==0) || ano1%400==0)?1:0;
-    if(mes1>mes2){
-        diasFaltam-=(365+bissexto);
-        mes1Temp = mes1;
-        while(mes1Temp<=12){
-            diasFaltam+= (((28+bissexto)*(mes1Temp==2) + (mes1Temp<8 && mes1Temp%2==0?30:31)*(mes1Temp<8 && (mes1Temp!=2)) + (mes1Temp>=8 && mes1Temp%2==0?31:30)*(mes1Temp>=8)) - dia1*(flag==1?1:0));
-            mes1Temp++;}
+    mes1Temp=mes1;
+    if(ano1<ano2){ //compensar shift inicial, comeca em ano diferente
+        bissexto = (ano1%4==0 && ano1%100!=0) || ano1%400==0; //checar se ano1 é bissexto
+        while(mes1Temp<=12){ // compensar ano que comeca
+            diasFaltam+=((28+bissexto)*(mes1Temp==2) + (mes1Temp%2==0 && mes1Temp<8?30:31)*(mes1Temp<8 && mes1Temp!=2)+(mes1Temp%2==0 && mes1Temp>=8?31:30)*(mes1Temp>=8))-dia1;
+            dia1=0;
+            mes1Temp++; //sai como 1
+        }
+        ano1++; // passou para o proximo ano com shift compensado
+        while(ano1<ano2){ // add anos seguintes
+            bissexto = (ano1%4==0 && ano1%100!=0) || ano1%400==0;
+            diasFaltam+=365+bissexto;
+            ano1++;
+        }
         mes1Temp=1;
-        while(mes1Temp<mes2){
-            diasFaltam+=((28+bissexto)*(mes1Temp==2) + (mes1Temp%2==0 && mes1Temp<8?30:31)*(mes1Temp<8 && mes1Temp!=2) + (mes1Temp%2==0 && mes1Temp>=8?31:30)*(mes1Temp>=8));
+        bissexto = (ano1%4==0 && ano1%100!=0) || ano1%400==0; //checar se ano1 é bissexto
+        while(mes1Temp<mes2){ // estamos no ano1=ano2
+            diasFaltam+=((28+bissexto)*(mes1Temp==2) + (mes1Temp%2==0 && mes1Temp<8?30:31)*(mes1Temp<8 && mes1Temp!=2)+(mes1Temp%2==0 && mes1Temp>=8?31:30)*(mes1Temp>=8));
             mes1Temp++;
         }
+        diasFaltam+=dia2; // shift de mes
     }
-    while(mes1 <mes2){
-     diasFaltam+= ((28+bissexto)*(mes1==2) + (mes1<8 && mes1%2==0?30:31)*(mes1<8 && mes1Temp!=2) + (mes1>=8 && mes1%2==0?31:30)*(mes1>=8)) - dia1*(flag==1?1:0);
-     mes1++;
+    else{
+        bissexto = (ano1%4==0 && ano1%100!=0) || ano1%400==0;
+        while(mes1Temp<mes2){ //comeca no mesmo ano, mas mes diferente
+            diasFaltam+=((28+bissexto)*(mes1Temp==2) + (mes1Temp%2==0 && mes1Temp<8?30:31)*(mes1Temp<8 && mes1Temp!=2)+(mes1Temp%2==0 && mes1Temp>=8?31:30)*(mes1Temp>=8)) -dia1;
+            dia1=0;
+        mes1Temp++;
+        }
+        diasFaltam+=dia2-dia1; // se mesmo ano e mes, note que para os outros casos dia1=0
     }
-    diasFaltam+= dia2 - dia1;
     printf("%d",diasFaltam);
 }
--- Semana3/Lista2.1/quemGanhou.c
+++ Semana3/Lista2.1/quemGanhou.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
-
 int main(){
    int scoreA=0;int scoreB=0;int rounds, cartaA, cartaB;
-   int somaA = 0;int somaB= 0;int desempateA = 0;int desempateB = 0;
-   int somaAtualA=0;int somaAtualB=0;
+   int somaAtualA, somaAtualB, divA, divB, cont;
    scanf("%d", &rounds);
-   while(rounds--){
+   scoreA=scoreB=0;
+   while(rounds>0?rounds--:0){
        scanf("%d %d", &cartaA, &cartaB);
-       desempateA+=cartaA;desempateB+=cartaB;
-       somaAtualA=0;somaAtualB=0;
-       while((cartaA/10)!=0){
-          cartaA = (cartaA%10)==0?cartaA/10:cartaA-cartaA%10;
+       somaAtualA=somaAtualB=0;
+       divA = divB = 1; // pegar o valor do numero em si
+       while(cartaA >= 1 || cartaB >=1){
+           if(cartaA > 0){
           somaAtualA +=(cartaA%10);
+          cartaA = (cartaA%10)==0?cartaA/10:cartaA-(cartaA%10);//pega as digitos se passar no teste ele pega 0
+           }
+           if(cartaB > 0){
+                somaAtualB +=(cartaB%10);
+                cartaB = (cartaB%10)==0?cartaB/10:cartaB-(cartaB%10);
+            }
        }
-       while(somaAtualA){
-
-       }
-
-       while((cartaB/10)!=0){
-          cartaB = (cartaB%10)==0?cartaB/10:cartaB-cartaB%10;
-          somaAtualB +=(cartaB%10);
-       }
-       somaAtualA%2==0?scoreA++:scoreA;
-       somaAtualB%2==0?scoreB++:scoreB;
-
+       for(cont=1;cont<=somaAtualA;cont++)
+               divA+=(somaAtualA % cont ==0 && cont>1)?1:0;
+       for(cont=1;cont<=somaAtualB;cont++)
+               divB+= (somaAtualB % cont == 0 && cont>1)?1:0;
+       if(somaAtualA==0) scoreA++;if(somaAtualB==0) scoreB++;
+       divA%2==0?scoreA++:scoreA;
+       divB%2==0?scoreB++:scoreB;
    }
+   divA=divB=1;
+   somaAtualA=somaAtualB=0;
+   cartaA=scoreA;cartaB=scoreB;
+    while(cartaA >= 1 || cartaB >= 1){
+        if(cartaA > 0){
+        somaAtualA +=(cartaA%10);
+        cartaA = (cartaA%10)==0?cartaA/10:cartaA-(cartaA%10);
+        }
+        if(cartaB > 0){
+        somaAtualB +=(cartaB%10);
+        cartaB = (cartaB%10)==0?cartaB/10:cartaB-(cartaB%10);
+        }
+    }
+   for(cont=1;cont<=somaAtualA;cont++)
+           divA+=(somaAtualA % cont==0 && cont > 1)?1:0;
+   for(cont=1;cont<=somaAtualB;cont++)
+           divB+=(somaAtualB % cont==0 && cont >1)?1:0;
+   scoreA = (somaAtualA==2)?scoreA-4:((divA==2 && somaAtualA%2==1)?scoreA+3:scoreA);
+   scoreB = (somaAtualB==2)?scoreB-4:((divB==2 && somaAtualB%2==1)?scoreB+3:scoreB);
+   printf("Quem ganhou foi aquele time.\n%d",(scoreA-scoreB)==0?1:(scoreA>scoreB?scoreA-scoreB:scoreB-scoreA));
 }
-hotel de Hillbert
+//hotel de Hillbert
