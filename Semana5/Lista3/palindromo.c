6798fa1b3fa00ec5cf2867c50bd66dcadd4afbc1
Enzo <enzogurgel15@gmail.com>
2021-06-27 18:30:45 -0300

    Added the string.h

--- Semana5/Lista3/palindromo.c
+++ Semana5/Lista3/palindromo.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 int main(){
     char palavra[50];int cont1 = 0, cont2=0, cont3=1,valido=1;
-    scanf("%c", palavra[cont1]);
-    while(palavra[cont1++]!='\n') // para pegar tamanho sem string.h
-        scanf("%c", palavra[cont1]);
-    palavra[cont1-1]='\0';cont1-=1; // porque referencia index nao na memoria
+    scanf("%49[^\n]s", palavra);int len = strlen(palavra);
     while(palavra[cont2]!='\0' && valido){
         // cont1-3 conta do final e cont2 checa se e simetrico
-        if(palavra[cont2]!=palavra[cont1-cont3]) valido=0;
+        if(palavra[cont2]!=palavra[len]) valido=0;
         cont2++;cont3++;
     }
     printf("%s", valido?"palìndromo":"não é palíndromo");
