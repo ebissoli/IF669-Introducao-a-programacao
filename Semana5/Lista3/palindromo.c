06655fc5701cbc3febb962decedfe36a9254f09f
Enzo <enzogurgel15@gmail.com>
2021-06-27 18:21:35 -0300

    palindromo v3

--- Semana5/Lista3/palindromo.c
+++ Semana5/Lista3/palindromo.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 int main(){
-    char palavra[50];int cont1 = 0, cont2=0, cont3=0,valido=1;
+    char palavra[50];int cont1 = 0, cont2=0, cont3=1,valido=1;
     scanf("%c", palavra[cont1]);
     while(palavra[cont1++]!='\n') // para pegar tamanho sem string.h
-        scanf(" %c", palavra[cont1]);
-    palavra[cont1-1]='\0';
-    if(cont1>2){
-        while(palavra[cont2]!='\0' && valido){
-            // cont1-3 conta do final e cont2 conta do inicio
-            if(palavra[cont2]!=palavra[cont1-cont3]) valido=0;
-            cont2++;cont3++;
-        }
-    printf("%s", valido && cont1>2?"palìndromo":"não é palíndromo");
+        scanf("%c", palavra[cont1]);
+    palavra[cont1-1]='\0';cont1-=1; // porque referencia index nao na memoria
+    while(palavra[cont2]!='\0' && valido){
+        // cont1-3 conta do final e cont2 checa se e simetrico
+        if(palavra[cont2]!=palavra[cont1-cont3]) valido=0;
+        cont2++;cont3++;
     }
-    else printf("%s", valido && cont1==1?"palindromo":"nao e palindromo");
+    printf("%s", valido?"palìndromo":"não é palíndromo");
     return 0;
 }
