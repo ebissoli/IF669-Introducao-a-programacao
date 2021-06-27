657990a1ea0a4f233f3a55e27a096c644c9f5eb6
Enzo <enzogurgel15@gmail.com>
2021-06-27 18:46:54 -0300

    Final edit of palindromo

--- Semana5/Lista3/palindromo.c
+++ Semana5/Lista3/palindromo.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char palavra[50];int cont1 = 0, cont2=0, cont3=1,valido=1;
-    scanf("%49[^\n]s", palavra);int len = strlen(palavra);
+    char palavra[50];int cont1 = 0, cont2=0, cont3=0,valido=1;
+    scanf("%49[^\n]s", palavra);int len = strlen(palavra)-1;
     while(palavra[cont2]!='\0' && valido){
         // cont1-3 conta do final e cont2 checa se e simetrico
-        if(palavra[cont2]!=palavra[len]) valido=0;
+        if(palavra[cont2]!=palavra[len-cont3]) valido=0;
         cont2++;cont3++;
     }
     printf("%s", valido?"palìndromo":"não é palíndromo");
